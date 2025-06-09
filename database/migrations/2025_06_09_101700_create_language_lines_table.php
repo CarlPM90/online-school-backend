ec2db5b0fcd16890cb9b42ae3bdbe9e93b7662dc
Carl <carl@edai.io>
2025-06-09 11:17:59 +0100

    Updating language lines table

---
+++ database/migrations/2025_06_09_101700_create_language_lines_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguageLinesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('language_lines', function (Blueprint $table) {
+            $table->id();
+            $table->string('group');
+            $table->string('key');
+            $table->text('text');
+            $table->timestamps();
+
+            $table->index(['group', 'key']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('language_lines');
+    }
+}
